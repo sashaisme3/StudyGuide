4b265b731fea870b657e92731375523463f8ae7f
sashaisme3 <sashamadatov333@gmail.com>
2025-08-16 15:02:57 -0700

    Update fetchContent.js

--- api/fetchContent.js
+++ api/fetchContent.js
@@ -2,11 +2,8 @@ import fetch from "node-fetch";
 
 export default async function handler(req, res) {
   try {
-    // The webpage to fetch
     const response = await fetch("https://en.wikipedia.org/wiki/Mathematics");
     const body = await response.text();
-
-    // Send it to the browser
     res.status(200).send(body);
   } catch (err) {
     res.status(500).send("Error fetching content");
